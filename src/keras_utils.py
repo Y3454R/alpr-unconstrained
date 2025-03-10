06cd719c2e0ff8770a734c90e5c47f9b5ba5e896
yeasar <syeasar.kuet@gmail.com>
2025-03-10 12:38:03 +0600

    :up: update: update license plate detection

--- src/keras_utils.py
+++ src/keras_utils.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 import time
@@ -8,111 +7,122 @@ from os.path import splitext
 from src.label import Label
 from src.utils import getWH, nms
 from src.projection_utils import getRectPts, find_T_matrix
+from keras.models import Model, model_from_json
 
 
-class DLabel (Label):
+class DLabel(Label):
 
-	def __init__(self,cl,pts,prob):
-		self.pts = pts
-		tl = np.amin(pts,1)
-		br = np.amax(pts,1)
-		Label.__init__(self,cl,tl,br,prob)
-
-def save_model(model,path,verbose=0):
-	path = splitext(path)[0]
-	model_json = model.to_json()
-	with open('%s.json' % path,'w') as json_file:
-		json_file.write(model_json)
-	model.save_weights('%s.h5' % path)
-	if verbose: print 'Saved to %s' % path
-
-def load_model(path,custom_objects={},verbose=0):
-	from keras.models import model_from_json
-
-	path = splitext(path)[0]
-	with open('%s.json' % path,'r') as json_file:
-		model_json = json_file.read()
-	model = model_from_json(model_json, custom_objects=custom_objects)
-	model.load_weights('%s.h5' % path)
-	if verbose: print 'Loaded from %s' % path
-	return model
+    def __init__(self, cl, pts, prob):
+        self.pts = pts
+        tl = np.amin(pts, 1)
+        br = np.amax(pts, 1)
+        Label.__init__(self, cl, tl, br, prob)
 
 
-def reconstruct(Iorig,I,Y,out_size,threshold=.9):
+def save_model(model, path, verbose=0):
+    path = splitext(path)[0]
+    model_json = model.to_json()
+    with open("%s.json" % path, "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("%s.h5" % path)
+    if verbose:
+        print("Saved to %s" % path)
 
-	net_stride 	= 2**4
-	side 		= ((208. + 40.)/2.)/net_stride # 7.75
 
-	Probs = Y[...,0]
-	Affines = Y[...,2:]
-	rx,ry = Y.shape[:2]
-	ywh = Y.shape[1::-1]
-	iwh = np.array(I.shape[1::-1],dtype=float).reshape((2,1))
+def load_model(path, custom_objects={}, verbose=0):
+    path = splitext(path)[0]
+    with open("%s.json" % path, "r") as json_file:
+        model_json = json_file.read()
 
-	xx,yy = np.where(Probs>threshold)
+    custom_objects.update({"Model": Model})  # Add Model to custom objects
+    model = model_from_json(model_json, custom_objects=custom_objects)
+    model.load_weights("%s.h5" % path)
 
-	WH = getWH(I.shape)
-	MN = WH/net_stride
+    if verbose:
+        print("Loaded from %s" % path)
 
-	vxx = vyy = 0.5 #alpha
+    return model
 
-	base = lambda vx,vy: np.matrix([[-vx,-vy,1.],[vx,-vy,1.],[vx,vy,1.],[-vx,vy,1.]]).T
-	labels = []
 
-	for i in range(len(xx)):
-		y,x = xx[i],yy[i]
-		affine = Affines[y,x]
-		prob = Probs[y,x]
+def reconstruct(Iorig, I, Y, out_size, threshold=0.9):
 
-		mn = np.array([float(x) + .5,float(y) + .5])
+    net_stride = 2**4
+    side = ((208.0 + 40.0) / 2.0) / net_stride  # 7.75
 
-		A = np.reshape(affine,(2,3))
-		A[0,0] = max(A[0,0],0.)
-		A[1,1] = max(A[1,1],0.)
+    Probs = Y[..., 0]
+    Affines = Y[..., 2:]
+    rx, ry = Y.shape[:2]
+    ywh = Y.shape[1::-1]
+    iwh = np.array(I.shape[1::-1], dtype=float).reshape((2, 1))
 
-		pts = np.array(A*base(vxx,vyy)) #*alpha
-		pts_MN_center_mn = pts*side
-		pts_MN = pts_MN_center_mn + mn.reshape((2,1))
+    xx, yy = np.where(Probs > threshold)
 
-		pts_prop = pts_MN/MN.reshape((2,1))
+    WH = getWH(I.shape)
+    MN = WH / net_stride
 
-		labels.append(DLabel(0,pts_prop,prob))
+    vxx = vyy = 0.5  # alpha
 
-	final_labels = nms(labels,.1)
-	TLps = []
+    base = lambda vx, vy: np.matrix(
+        [[-vx, -vy, 1.0], [vx, -vy, 1.0], [vx, vy, 1.0], [-vx, vy, 1.0]]
+    ).T
+    labels = []
 
-	if len(final_labels):
-		final_labels.sort(key=lambda x: x.prob(), reverse=True)
-		for i,label in enumerate(final_labels):
+    for i in range(len(xx)):
+        y, x = xx[i], yy[i]
+        affine = Affines[y, x]
+        prob = Probs[y, x]
 
-			t_ptsh 	= getRectPts(0,0,out_size[0],out_size[1])
-			ptsh 	= np.concatenate((label.pts*getWH(Iorig.shape).reshape((2,1)),np.ones((1,4))))
-			H 		= find_T_matrix(ptsh,t_ptsh)
-			Ilp 	= cv2.warpPerspective(Iorig,H,out_size,borderValue=.0)
+        mn = np.array([float(x) + 0.5, float(y) + 0.5])
 
-			TLps.append(Ilp)
+        A = np.reshape(affine, (2, 3))
+        A[0, 0] = max(A[0, 0], 0.0)
+        A[1, 1] = max(A[1, 1], 0.0)
 
-	return final_labels,TLps
-	
+        pts = np.array(A * base(vxx, vyy))  # *alpha
+        pts_MN_center_mn = pts * side
+        pts_MN = pts_MN_center_mn + mn.reshape((2, 1))
 
-def detect_lp(model,I,max_dim,net_step,out_size,threshold):
+        pts_prop = pts_MN / MN.reshape((2, 1))
 
-	min_dim_img = min(I.shape[:2])
-	factor 		= float(max_dim)/min_dim_img
+        labels.append(DLabel(0, pts_prop, prob))
 
-	w,h = (np.array(I.shape[1::-1],dtype=float)*factor).astype(int).tolist()
-	w += (w%net_step!=0)*(net_step - w%net_step)
-	h += (h%net_step!=0)*(net_step - h%net_step)
-	Iresized = cv2.resize(I,(w,h))
+    final_labels = nms(labels, 0.1)
+    TLps = []
 
-	T = Iresized.copy()
-	T = T.reshape((1,T.shape[0],T.shape[1],T.shape[2]))
+    if len(final_labels):
+        final_labels.sort(key=lambda x: x.prob(), reverse=True)
+        for i, label in enumerate(final_labels):
 
-	start 	= time.time()
-	Yr 		= model.predict(T)
-	Yr 		= np.squeeze(Yr)
-	elapsed = time.time() - start
+            t_ptsh = getRectPts(0, 0, out_size[0], out_size[1])
+            ptsh = np.concatenate(
+                (label.pts * getWH(Iorig.shape).reshape((2, 1)), np.ones((1, 4)))
+            )
+            H = find_T_matrix(ptsh, t_ptsh)
+            Ilp = cv2.warpPerspective(Iorig, H, out_size, borderValue=0.0)
 
-	L,TLps = reconstruct(I,Iresized,Yr,out_size,threshold)
+            TLps.append(Ilp)
 
-	return L,TLps,elapsed
+    return final_labels, TLps
+
+
+def detect_lp(model, I, max_dim, net_step, out_size, threshold):
+
+    min_dim_img = min(I.shape[:2])
+    factor = float(max_dim) / min_dim_img
+
+    w, h = (np.array(I.shape[1::-1], dtype=float) * factor).astype(int).tolist()
+    w += (w % net_step != 0) * (net_step - w % net_step)
+    h += (h % net_step != 0) * (net_step - h % net_step)
+    Iresized = cv2.resize(I, (w, h))
+
+    T = Iresized.copy()
+    T = T.reshape((1, T.shape[0], T.shape[1], T.shape[2]))
+
+    start = time.time()
+    Yr = model.predict(T)
+    Yr = np.squeeze(Yr)
+    elapsed = time.time() - start
+
+    L, TLps = reconstruct(I, Iresized, Yr, out_size, threshold)
+
+    return L, TLps, elapsed
